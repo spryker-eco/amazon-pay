101a1a03acac1fa34373310dc21499e5518704ca
Andrey Tkachenko <andrey.tkachenko@spryker.com>
2017-09-26 16:49:56 +0200

    ECO-759 orders are assigned to logged in user

---
+++ src/SprykerEco/Zed/Amazonpay/Dependency/Client/AmazonpayToCustomerBridge.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Apache OSL-2
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace SprykerEco\Zed\Amazonpay\Dependency\Client;
+
+use Generated\Shared\Transfer\QuoteTransfer;
+
+class AmazonpayToCustomerBridge implements AmazonpayToCustomerInterface
+{
+
+    /**
+     * @var \Spryker\Client\Customer\CustomerClientInterface
+     */
+    protected $customerClient;
+
+    /**
+     * @param \Spryker\Client\Customer\CustomerClientInterface $customerClient
+     */
+    public function __construct($customerClient)
+    {
+        $this->customerClient = $customerClient;
+    }
+
+    /**
+     * @return \Generated\Shared\Transfer\CustomerTransfer|null
+     */
+    public function getCustomer()
+    {
+        return $this->customerClient->getCustomer();
+    }
+
+}
---
+++ src/SprykerEco/Zed/Amazonpay/Dependency/Client/AmazonpayToCustomerInterface.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Apache OSL-2
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace SprykerEco\Zed\Amazonpay\Dependency\Client;
+
+use Generated\Shared\Transfer\QuoteTransfer;
+
+interface AmazonpayToCustomerInterface
+{
+
+    /**
+     * Gets current logged in customer.
+     *
+     * @api
+     *
+     * @return \Generated\Shared\Transfer\CustomerTransfer|null
+     */
+    public function getCustomer();
+
+}
