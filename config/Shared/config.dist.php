80942a8d403af66be3d240d4eb95a77912b1146d
Andrey Tkachenko <andrey.tkachenko@spryker.com>
2017-09-28 17:26:08 +0200

    ECO-337 templates update, default config update

---
+++ config/Shared/config.dist.php
@@ -0,0 +1,35 @@
+<?php
+
+use Spryker\Shared\Acl\AclConstants;
+use Spryker\Shared\Sales\SalesConstants;
+use SprykerEco\Shared\Amazonpay\AmazonpayConstants;
+
+$config[AmazonpayConstants::CLIENT_ID] = '';
+$config[AmazonpayConstants::CLIENT_SECRET] = '';
+$config[AmazonpayConstants::SELLER_ID] = '';
+$config[AmazonpayConstants::ACCESS_KEY_ID] = '';
+$config[AmazonpayConstants::SECRET_ACCESS_KEY] = '';
+$config[AmazonpayConstants::REGION] = 'DE';
+$config[AmazonpayConstants::STORE_NAME] = 'Demo shop';
+$config[AmazonpayConstants::SANDBOX] = true;
+$config[AmazonpayConstants::AUTH_TRANSACTION_TIMEOUT] = 1000;
+$config[AmazonpayConstants::CAPTURE_NOW] = true;
+$config[AmazonpayConstants::ERROR_REPORT_LEVEL] = 'ERRORS_ONLY';
+$config[AmazonpayConstants::PAYMENT_REJECT_ROUTE] = 'cart';
+$config[AmazonpayConstants::WIDGET_SCRIPT_PATH] = 'https://static-eu.payments-amazon.com/OffAmazonPayments/eur/lpa/js/Widgets.js';
+$config[AmazonpayConstants::WIDGET_SCRIPT_PATH_SANDBOX] = 'https://static-eu.payments-amazon.com/OffAmazonPayments/eur/sandbox/lpa/js/Widgets.js';
+$config[AmazonpayConstants::WIDGET_POPUP_LOGIN] = true;
+$config[AmazonpayConstants::WIDGET_BUTTON_TYPE] = AmazonpayConstants::WIDGET_BUTTON_TYPE_FULL;
+$config[AmazonpayConstants::WIDGET_BUTTON_SIZE] = AmazonpayConstants::WIDGET_BUTTON_SIZE_MEDIUM;
+$config[AmazonpayConstants::WIDGET_BUTTON_COLOR] = AmazonpayConstants::WIDGET_BUTTON_COLOR_DARK_GRAY;
+
+$config[SalesConstants::PAYMENT_METHOD_STATEMACHINE_MAPPING][AmazonpayConstants::PAYMENT_METHOD] =
+    $config[AmazonpayConstants::CAPTURE_NOW] ? 'AmazonpayPaymentSync01' : 'AmazonpayPaymentAsync01';
+
+$config[AclConstants::ACL_USER_RULE_WHITELIST][] =
+    [
+        'bundle' => 'amazonpay',
+        'controller' => 'ipn',
+        'action' => 'endpoint',
+        'type' => 'allow',
+    ];
