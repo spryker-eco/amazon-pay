b069d043ea6335cf00c7145ed1b2c29b21babfd5
Andrey Tkachenko <andrey.tkachenko@spryker.com>
2018-03-12 11:46:34 +0100

    ECO-1134 unit tests debug

--- tests/SprykerEcoTest/Zed/AmazonPay/Business/AmazonpayFacadeAbstractTest.php
+++ tests/SprykerEcoTest/Zed/AmazonPay/Business/AmazonpayFacadeAbstractTest.php
@@ -20,6 +20,7 @@ use Orm\Zed\Sales\Persistence\SpySalesOrderAddress;
 use Orm\Zed\Sales\Persistence\SpySalesOrderAddressQuery;
 use Orm\Zed\Sales\Persistence\SpySalesOrderItemQuery;
 use Orm\Zed\Sales\Persistence\SpySalesOrderQuery;
+use Orm\Zed\Shipment\Persistence\SpyShipmentCarrier;
 use Orm\Zed\Shipment\Persistence\SpyShipmentMethod;
 use Orm\Zed\Shipment\Persistence\SpyShipmentMethodQuery;
 use SprykerEco\Shared\AmazonPay\AmazonPayConfig;
@@ -111,19 +112,20 @@ class AmazonpayFacadeAbstractTest extends Test
             $ids[] = $shipmentMethod->getIdShipmentMethod();
         }
 
+        $carrier = new SpyShipmentCarrier();
+        $carrier->setName('test-carrier');
+        $carrier->save();
+
         if (count($ids) < $count) {
-            for ($i = count($ids); $i < $count; $i++) {
+            for ($i = 0; $i < $count; $i++) {
                 $shipmentMethod = new SpyShipmentMethod();
-                $shipmentMethod->save();
+                $shipmentMethod
+                    ->setName($i)
+                    ->setFkShipmentCarrier($carrier->getIdShipmentCarrier())
+                    ->save();
                 $ids[] = $shipmentMethod->getIdShipmentMethod();
             }
         }
-
-        $shipmentMethods = SpyShipmentMethodQuery::create()
-            ->limit($count)
-            ->find();
-
-        throw new \Exception(var_export($shipmentMethods->toArray(), true));
 
         return $ids;
     }
