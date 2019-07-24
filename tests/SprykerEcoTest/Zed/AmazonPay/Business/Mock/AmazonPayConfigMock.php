fb09464a7bb7b8b0caa6a595e64d211d725ac37a
vhrychenko <aptah199494@gmail.com>
2019-07-24 12:54:26 +0300

    Try to fix tests

--- tests/SprykerEcoTest/Zed/AmazonPay/Business/Mock/AmazonPayConfigMock.php
+++ tests/SprykerEcoTest/Zed/AmazonPay/Business/Mock/AmazonPayConfigMock.php
@@ -48,4 +48,28 @@ class AmazonPayConfigMock extends AmazonPayConfig
 
         return parent::getAuthTransactionTimeout();
     }
+
+    /**
+     * @return bool
+     */
+    public function getSuccessPaymentUrl(): string
+    {
+        if (isset($this->additionalConfig[AmazonPayConstants::SUCCESS_PAYMENT_URL])) {
+            return $this->additionalConfig[AmazonPayConstants::SUCCESS_PAYMENT_URL];
+        }
+
+        return parent::getCaptureNow();
+    }
+
+    /**
+     * @return bool
+     */
+    public function getFailurePaymentUrl(): string
+    {
+        if (isset($this->additionalConfig[AmazonPayConstants::FAILURE_PAYMENT_URL])) {
+            return $this->additionalConfig[AmazonPayConstants::FAILURE_PAYMENT_URL];
+        }
+
+        return parent::getCaptureNow();
+    }
 }
