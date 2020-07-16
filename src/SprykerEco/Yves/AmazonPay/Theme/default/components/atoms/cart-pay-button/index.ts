0ab531160fe607079664ed05279c2395efb80b16
maslov <dev.maslov.a@gmail.com>
2020-07-16 18:32:05 +0300

    TE-6081: Introduced webpackChunkName commentary.

--- src/SprykerEco/Yves/AmazonPay/Theme/default/components/atoms/cart-pay-button/index.ts
+++ src/SprykerEco/Yves/AmazonPay/Theme/default/components/atoms/cart-pay-button/index.ts
@@ -1,2 +1,5 @@
 import register from 'ShopUi/app/registry';
-export default register('cart-pay-button', () => import(/* webpackMode: "" */'./cart-pay-button'));
+export default register('cart-pay-button', () => import(
+    /* webpackMode: "lazy" */
+    /* webpackChunkName: "cart-pay-button" */
+    './cart-pay-button'));
